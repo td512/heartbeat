a91747333237b18f053f8c789a6b638815903a15
Theo M <td512@td512.io>
2018-04-04 00:31:10 +1200

    clean up finger script

--- finger.rb
+++ finger.rb
@@ -1,44 +1,26 @@
 #!/usr/bin/ruby
+# frozen_string_literal: true
+
 require 'yaml'
 require 'tty-table'
-
 
 @content = YAML.load_file(File.join(__dir__, '/status.yaml'))
 
 service = gets.chomp
-if service.include?('@')
-  service = service.split('@').first
-end
+service = service.split('@').first if service.include?('@')
 
-if service == "all" or service == ""
 serviceTable = TTY::Table.new header: ['Server', 'Service', 'Status', 'Last check', 'Response time']
-serviceRows  = Array.new
-  @content.each do |serverName, services|
-    services.each do |serviceName, serviceData|
-      serviceTable << [
-        serverName,
-        serviceName,
-        serviceData['status'],
-        "#{(Time.now - serviceData['lastCheck']).floor} sec ago",
-        "#{serviceData['responseTime']} ms"
-      ]
+@content.each do |serverName, services|
+  services.each do |serviceName, serviceData|
+    next unless serverName.casecmp(service.downcase).zero? ||
+                (service == 'all') || (service == '') || serverName.downcase.include?(service.downcase)
+    serviceTable << [
+      serverName,
+      serviceName,
+      serviceData['status'],
+      "#{(Time.now - serviceData['lastCheck']).floor} sec ago",
+      "#{serviceData['responseTime']} ms"
+    ]
   end
 end
 puts serviceTable.render(:ascii)
-else
-serviceTable = TTY::Table.new header: ['Server', 'Service', 'Status', 'Last check', 'Response time']
-serviceRows  = Array.new
-  @content.each do |serverName, services|
-    services.each do |serviceName, serviceData|
-      next unless serverName.downcase == service.downcase
-      serviceTable << [
-        serverName,
-        serviceName,
-        serviceData['status'],
-        "#{(Time.now - serviceData['lastCheck']).floor} sec ago",
-        "#{serviceData['responseTime']} ms"
-      ]
-  end
-end
-puts serviceTable.render(:ascii)
-end
