d1e8f5d26103cdcad6e0be23b1c81ed9a1c92c07
Theo M <td512@td512.io>
2018-04-03 22:54:36 +1200

    fixes static alert banner

--- heartbeat_app.rb
+++ heartbeat_app.rb
@@ -5,12 +5,13 @@ require 'tty-table'
 require 'yaml'
 
 get '/' do
-  @title      = 'Service Status - Monarch'
-  @content    = YAML.load_file('status.yaml')
-  @color      = "greens"
-  @statusText = "Operational"
-  @copyright  = 'Powered by <a href="https://github.com/td512/heartbeat" class="links">Monarch Heartbeat</a>.'
-
+  @title         = 'Service Status - Monarch'
+  @content       = YAML.load_file('status.yaml')
+  @color         = "greens"
+  @statusText    = "Operational"
+  @copyright     = 'Powered by <a href="https://github.com/td512/heartbeat" class="links">Monarch Heartbeat</a>.'
+  @bannerText    = "All systems are operational"
+  @service_color = "success"
   if request.user_agent.match?(/wget|curl/i)
     content_type :text
     erb :index_text
